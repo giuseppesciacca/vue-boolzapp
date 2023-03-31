385fa61f4940c1ee63e890b597b795e93fe0faa3
giuseppesciacca <sciaccagi@gmail.com>
2023-03-31 16:36:48 +0200

    [add] contacts with js

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,10 +11,11 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
+            activeChat: 0,
             contacts: [
                 {
                     name: 'Michele',
-                    avatar: './img/avatar_1.jpg',
+                    avatar: '/img/avatar_1.jpg',
                     visible: true,
                     messages: [
                         {
@@ -36,7 +37,7 @@ createApp({
                 },
                 {
                     name: 'Fabio',
-                    avatar: './img/avatar_2.jpg',
+                    avatar: '/img/avatar_2.jpg',
                     visible: true,
                     messages: [
                         {
@@ -58,7 +59,7 @@ createApp({
                 },
                 {
                     name: 'Samuele',
-                    avatar: './img/avatar_3.jpg',
+                    avatar: '/img/avatar_3.jpg',
                     visible: true,
                     messages: [
                         {
@@ -80,7 +81,7 @@ createApp({
                 },
                 {
                     name: 'Alessandro B.',
-                    avatar: './img/avatar_4.jpg',
+                    avatar: '/img/avatar_4.jpg',
                     visible: true,
                     messages: [
                         {
@@ -97,7 +98,7 @@ createApp({
                 },
                 {
                     name: 'Alessandro L.',
-                    avatar: './img/avatar_5.jpg',
+                    avatar: '/img/avatar_5.jpg',
                     visible: true,
                     messages: [
                         {
@@ -114,7 +115,7 @@ createApp({
                 },
                 {
                     name: 'Claudia',
-                    avatar: './img/avatar_5.jpg',
+                    avatar: '/img/avatar_5.jpg',
                     visible: true,
                     messages: [
                         {
@@ -136,7 +137,7 @@ createApp({
                 },
                 {
                     name: 'Federico',
-                    avatar: './img/avatar_7.jpg',
+                    avatar: '/img/avatar_7.jpg',
                     visible: true,
                     messages: [
                         {
@@ -153,7 +154,7 @@ createApp({
                 },
                 {
                     name: 'Davide',
-                    avatar: './img/avatar_8.jpg',
+                    avatar: '/img/avatar_8.jpg',
                     visible: true,
                     messages: [
                         {
