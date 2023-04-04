f992e6287f576f7c244c777ec76dc7cdacd702a7
giuseppesciacca <sciaccagi@gmail.com>
2023-04-04 13:19:25 +0200

    [clean] code and [add] comments

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,3 @@
-/* Milestone 1
-Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse
-Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
-
-Milestone 2
-Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
-Click sul contatto mostra la conversazione del contatto cliccato
-
-Milestone 3
-Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando
-“enter” il testo viene aggiunto al thread sopra, come messaggio verde
-Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
-
-Milestone 4
-Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
-
-Milestone 5 - opzionale
-Cancella messaggio: cliccando sul messaggio appare un menu a tendina che permette di cancellare il messaggio selezionato.
-Visualizzazione ora e ultimo messaggio inviato/ricevuto nella lista dei contatti*/
-
 /* BONUS MANCANTI
 sotto al nome del contatto nella parte in alto a destra, cambiare l'indicazione dello stato: visualizzare il testo "sta scrivendo..."
 nel timeout in cui il pc risponde, poi mantenere la scritta "online" per un paio di secondi e infine visualizzare "ultimo accesso alle xx:yy" con l'orario corretto
@@ -310,12 +290,27 @@ createApp({
             return dateStr
         },
 
+        /**
+         * 
+         * @param {int} activeChat 
+         * svuota l'array dei messaggi
+         */
         deleteAllMessage(activeChat) {
             this.contacts[activeChat].messages = [];
         },
+
+        /**
+         * 
+         * @param {int} activeChat 
+         * elimina il messaggio selezionato
+         */
         deleteChat(activeChat) {
             this.contacts.splice(this.contacts[activeChat], 1);
         },
+
+        /**
+         * cambia icona e testo dell'active notification
+         */
         activeNotification() {
             const activeNotiEl = document.getElementById('text_active_notification');
             const slashBellEl = document.querySelector('.fa-bell-slash');
@@ -327,6 +322,10 @@ createApp({
             disactiveNotiEl.classList.toggle('d-none')
             bellEl.classList.toggle('d-none')
         },
+
+        /**
+         * Aggiunge un contatto all'array contacts e chiude la modale
+         */
         newContact() {
             const closeModalEl = document.getElementById('close_modal');
 
@@ -341,6 +340,12 @@ createApp({
             }
             closeModalEl.click();
         },
+
+        /**
+         * 
+         * @param {string} fullName 
+         * @returns first letters of name
+         */
         getFirstLetters(fullName) {
             const firstLetterArray = fullName.split(' ').map((word) => {
                 return word.charAt(0)
